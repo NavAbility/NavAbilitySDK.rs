702a15334b3c17c593081401889b7de89bf7c1e9
dehann <dehann.fourie@gmail.com>
2025-02-11 20:35:17 -0800

    mv post_to_nvaapi to common

--- src/services/Worker.rs
+++ src/services/Worker.rs
@@ -1,56 +1,55 @@
 
 #[cfg(any(feature = "tokio", feature = "wasm", feature = "blocking"))]
 use crate::{
-  Serialize,
+  // Serialize,
   GraphQLQuery,
   QueryBody,
-  NavAbilityClient,
   Error,
   GQLRequestError,
   GQLResponseEmptyError,
   check_deser,
   check_query_response_data,
   to_console_error,
-  print_type_of,
+  NavAbilityClient,
+  post_to_nvaapi,
   StartWorker,
   // start_worker
 };
 
-use serde::Deserialize;
+// use serde::{Serialize,,Deserialize};
+
+// use std::any::type_name;
 
 
-
-#[cfg(any(feature = "tokio", feature = "wasm", feature = "blocking"))]
-pub async fn post_to_nvaapi<
-  Q: Serialize,
-  R: for<'de> Deserialize<'de>,
-  T
->(
-  nvacl: &NavAbilityClient,
-  request_body: Q,
-  fn_modifier: fn(R) -> T
-) -> Result<T, Box<dyn Error>> {
-    use serde::{Deserialize, Serialize};
-
-  let req_res = nvacl.client
-  .post(&nvacl.apiurl)
-  .json(&request_body)
-  .send().await;
+// #[cfg(any(feature = "tokio", feature = "wasm", feature = "blocking"))]
+// pub async fn post_to_nvaapi<
+//   V: Serialize,
+//   R: for<'de> Deserialize<'de>,
+//   T
+// >(
+//   nvacl: &NavAbilityClient,
+//   request_body: crate::QueryBody<V>,
+//   fn_modifier: fn(R) -> T
+// ) -> Result<T, Box<dyn Error>> {
+//   let req_res = nvacl.client
+//   .post(&nvacl.apiurl)
+//   .json(&request_body)
+//   .send().await;
   
-  if let Err(ref re) = req_res {
-    let erm = format!("API request error for {:?}: {:?}",  print_type_of(&request_body), &re);
-    to_console_error(&erm);
-    return Err(Box::new(GQLRequestError { details: erm }));
-  }
+//   if let Err(ref re) = req_res {
+//     let erm = format!("API request error for {:?}: {:?}",  type_name::<V>(), &re);
+//     to_console_error(&erm);
+//     return Err(Box::new(GQLRequestError { details: erm }));
+//   }
   
-  // generic transport and serde error checks
-  let response_body = check_deser::<R>(
-    req_res?.json().await
-  );
+//   // generic transport and serde error checks
+//   let response_body = check_deser::<R>(
+//     req_res?.json().await
+//   );
   
-  // unwrap StartWorker query response during error checks
-  return check_query_response_data(response_body, fn_modifier);
-}
+//   // unwrap StartWorker query response during error checks
+//   return check_query_response_data(response_body, fn_modifier);
+// }
 
 
 
@@ -69,7 +68,7 @@ pub async fn post_start_worker(
   let request_body = StartWorker::build_query(variables);
   
   return post_to_nvaapi::<
-    crate::QueryBody<crate::start_worker::Variables>,
+    crate::start_worker::Variables,
     crate::start_worker::ResponseData,
     String
   >(
