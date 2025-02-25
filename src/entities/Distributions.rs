0930b9f09680eb823069abba0d46ea73a3399b29
dehann <dehann.fourie@gmail.com>
2025-02-24 18:04:42 -0800

    traits Distribution FactorType

--- src/entities/Distributions.rs
+++ src/entities/Distributions.rs
@@ -1,9 +1,25 @@
 
 
+pub trait Distribution<'a>: 'a {
+    fn new(params: Vec<&'a [f64]>) -> Self;
+}
+
+
 /// Multidimensional normal distribution specified by means and a covariance matrix.
 #[allow(non_snake_case)]
 #[derive(Debug, Clone)]
 pub struct FullNormal<'a> {
     pub mean: &'a [f64],
     pub covr: &'a [f64],
+}
+
+impl<'a> Distribution<'a> for FullNormal<'a> {
+    fn new(
+        params: Vec<&'a [f64]>
+    ) -> Self {
+        Self {
+            mean: params[0],
+            covr: params[1],
+        }
+    }
 }
--- src/services/Factors.rs
+++ src/services/Factors.rs
@@ -1,170 +1,137 @@
-
 use uuid::Uuid;
 
 use chrono::{
-  DateTime,
+  DateTime, 
   Utc
 };
 
-use base64::{Engine as _, engine::{self, general_purpose}, alphabet};
-
-
-use crate::{
-  FullNormal,
-  PriorPoint2,
-  PriorPoint3,
-  PriorPose2,
-  PriorPose3,
-  Point2Point2,
-  Point3Point3,
-  Pose2Pose2,
-  Pose3Pose3,
+use base64::{
+  alphabet,
+  engine::{self, general_purpose},
+  Engine as _,
 };
 
 
+use crate::entities::Distributions::Distribution;
+use crate::{
+  entities::Factors::{FactorDFG, FunctionData},
+  FullNormal, 
+  Point2Point2, 
+  Point3Point3, 
+  Pose2Pose2, 
+  Pose3Pose3, 
+  PriorPoint2, 
+  PriorPoint3,
+  PriorPose2, PriorPose3,
+};
+
+
+#[cfg(any(feature = "tokio", feature = "wasm", feature = "blocking"))]
+use crate::entities::ClientDFG::NavAbilityDFG;
+
 
 #[allow(non_snake_case)]
-impl<'a> PriorPoint2<FullNormal<'a>> {
-  pub fn new(
-    Z: FullNormal<'a>
-  ) -> Self {
+trait FactorType<'a, T: Distribution<'a>> {
+  fn new(Z: T) -> Self;
+}
+
+
+
+impl<'a, T: Distribution<'a>> FactorType<'a, T> for PriorPoint2<T> {
+  fn new(Z: T) -> Self {
     Self {
-      Z: Z
+      Z
     }
   }
 }
 
-
+// #[allow(non_snake_case)]
+// impl<'a> PriorPoint2<FullNormal<'a>> {
+//   pub fn new(Z: FullNormal<'a>) -> Self {
+//     Self { Z: Z }
+//   }
+// }
 
 #[allow(non_snake_case)]
 impl<'a> PriorPoint3<FullNormal<'a>> {
-  pub fn new(
-    Z: FullNormal<'a>
-  ) -> Self {
-    Self {
-      Z: Z
-    }
+  pub fn new(Z: FullNormal<'a>) -> Self {
+    Self { Z: Z }
   }
 }
-
 
 #[allow(non_snake_case)]
 impl<'a> PriorPose2<FullNormal<'a>> {
-  pub fn new(
-    Z: FullNormal<'a>
-  ) -> Self {
-    Self {
-      Z: Z
-    }
+  pub fn new(Z: FullNormal<'a>) -> Self {
+    Self { Z: Z }
   }
 }
-
 
 #[allow(non_snake_case)]
 impl<'a> PriorPose3<FullNormal<'a>> {
-  pub fn new(
-    Z: FullNormal<'a>
-  ) -> Self {
-    Self {
-      Z: Z
-    }
+  pub fn new(Z: FullNormal<'a>) -> Self {
+    Self { Z: Z }
   }
 }
-
 
 #[allow(non_snake_case)]
 impl<'a> Point2Point2<FullNormal<'a>> {
-  pub fn new(
-    Z: FullNormal<'a>
-  ) -> Self {
-    Self {
-      Z: Z
-    }
+  pub fn new(Z: FullNormal<'a>) -> Self {
+    Self { Z: Z }
   }
 }
-
 
 #[allow(non_snake_case)]
 impl<'a> Point3Point3<FullNormal<'a>> {
-  pub fn new(
-    Z: FullNormal<'a>
-  ) -> Self {
-    Self {
-      Z: Z
-    }
+  pub fn new(Z: FullNormal<'a>) -> Self {
+    Self { Z: Z }
   }
 }
-
 
 #[allow(non_snake_case)]
 impl<'a> Pose2Pose2<FullNormal<'a>> {
-  pub fn new(
-    Z: FullNormal<'a>
-  ) -> Self {
-    Self {
-      Z: Z
-    }
+  pub fn new(Z: FullNormal<'a>) -> Self {
+    Self { Z: Z }
   }
 }
-
 
 #[allow(non_snake_case)]
 impl<'a> Pose3Pose3<FullNormal<'a>> {
-  pub fn new(
-    Z: FullNormal<'a>
-  ) -> Self {
-    Self {
-      Z: Z
-    }
+  pub fn new(Z: FullNormal<'a>) -> Self {
+    Self { Z: Z }
   }
 }
-
 
 
 
 // =======================================================
 
 
-use crate::{
-  FunctionData,
-  FactorDFG
-};
-
-
-fn assemble_factor_name(
-  ovlb: Vec<&str>
-) -> String {
+fn assemble_factor_name(ovlb: Vec<&str>) -> String {
   let mut flb = "".to_string();
   for o in ovlb {
     flb += o;
-  } 
+  }
   flb += "_";
   flb += &(Uuid::new_v4().to_string()[0..4]);
   
   return flb;
 }
 
-
-fn get_fnc_name(
-  fnc: &str
-) -> String {
+fn get_fnc_name(fnc: &str) -> String {
   let parts = fnc.split(".");
-  let mut t= "";
+  let mut t = "";
   for part in parts {
     t = part;
   }
   return t.to_owned();
 }
 
-
-
-
 impl FunctionData {
   pub fn new(
     fnc: &str,
     multihypo: Option<Vec<f64>>,
     nullhypo: Option<f64>,
-    inflation: Option<f64>
+    inflation: Option<f64>,
   ) -> Self {
     let mut fd = Self::default();
     fd.fnc = fnc.to_owned();
@@ -173,23 +140,54 @@ impl FunctionData {
     fd.inflation = inflation.unwrap_or(3.0);
     return fd;
   }
-
-  pub fn to_json(
-    &self,
-  ) -> String {
+  
+  pub fn to_json(&self) -> String {
     serde_json::to_string(self).unwrap().to_string()
   }
-
-  pub fn to_string_b64(
-    &self,
-  ) -> String { 
+  
+  pub fn to_string_b64(&self) -> String {
     general_purpose::STANDARD.encode(self.to_json().to_string())
   }
 }
 
-
-
 impl FactorDFG {
+  /// Create a new factor
+  /// # Arguments
+  /// * `varlbls` - variable labels is a ordered vector of variable label strings, e.g. ["x1", "x2"]
+  /// * `fnctype` - function type, e.g. "RoME.Pose3Pose3", "RoME.PriorPose2", see Caesar.jl.
+  /// * `tags` - tags are additional strings to help identify the factor
+  /// * `timestamp` - timestamp, in DateTime<Utc>, default is now
+  /// * `nstime` - nstime, also known as "time since epoch" in nanoseconds
+  /// * # Returns
+  /// * `FactorDFG` - a new factor
+  /// # Example
+  /// ```
+  /// use navabilitysdk::services::Factors;
+  /// use chrono::{DateTime, Utc};
+  /// let f = Factors::new(vec!["x1", "x2"], "RoME.Pose3Pose3", vec!["ODOMETRY","BODY_FRAME"], Some(Utc::now()));
+  /// ```
+  /// # Note
+  /// * This is a simplified version of the original function, which is more complex and has more options.
+  pub fn new(
+    varlbls: Vec<&str>,
+    fnctype: &str,
+    tags: Vec<&str>,
+    timestamp: Option<DateTime<Utc>>,
+    nstime: Option<usize>,
+  ) -> Self {
+    return Self::new_more(
+      varlbls, fnctype, tags, timestamp, nstime, None, None, None, None,
+    );
+  }
+  
+  /// Create a new factor with more expansive inputs.  See `new` for details.
+  /// # Arguments
+  /// solvable - solvable, default is 1 which means the solver will attempt compute numerical values
+  /// multihypo - multihypo, default is empty vector.  Allows fractional hypotheses, see Caesar.jl Docs for details
+  /// nullhypo - nullhypo, default is 0.0.  Allows fractional null hypotheses, see Caesar.jl Docs for details
+  /// inflation - inflation, default is 3.0.  Allows inflation of the covariance, see Caesar.jl Docs for details
+  /// # Returns
+  /// * `FactorDFG` - a new factor
   pub fn new_more(
     varlbls: Vec<&str>,
     fnctype: &str,
@@ -199,7 +197,7 @@ impl FactorDFG {
     solvable: Option<i64>,
     multihypo: Option<Vec<f64>>,
     nullhypo: Option<f64>,
-    inflation: Option<f64>
+    inflation: Option<f64>,
   ) -> Self {
     let mut f = FactorDFG::default();
     
@@ -214,16 +212,11 @@ impl FactorDFG {
       }
     }
     // default on create, also deser is different use-case
-    f.timestamp = Some(timestamp.unwrap_or(Utc::now())); 
-    f.nstime = Some(format!("{}",nstime.unwrap_or(0)));
+    f.timestamp = Some(timestamp.unwrap_or(Utc::now()));
+    f.nstime = Some(format!("{}", nstime.unwrap_or(0)));
     f.solvable = Some(solvable.unwrap_or(0));
-
-    let fdata = FunctionData::new(
-      &f.fnctype.to_string(),
-      multihypo,
-      nullhypo,
-      inflation,
-    );
+    
+    let fdata = FunctionData::new(&f.fnctype.to_string(), multihypo, nullhypo, inflation);
     // FIXME, should not be json'd so early: JuliaRobotics/DistributedFactorGraphs.jl#1118
     f.data = Some(fdata.to_json());
     
@@ -232,6 +225,13 @@ impl FactorDFG {
 }
 
 
+#[cfg(any(feature = "tokio", feature = "wasm", feature = "blocking"))]
+pub fn post_add_factor(
+  nvacl: &NavAbilityDFG,
+  factor: FactorDFG,
+) {
+  todo!()
+}
 
 
 
@@ -255,7 +255,7 @@ impl FactorDFG {
 //     fnctype = getFncTypeName(fnc)
 
 //     union!(tags, [:FACTOR])
-//     # create factor 
+//     # create factor
 //     factor = FactorDFG(;
 //         label,
 //         tags,
