1067384fdd094a32cd8c522f6de42b4c91e3b7b4
dehann <dehann.fourie@gmail.com>
2025-03-02 18:03:44 -0800

    refac post_to_nvaapi_cb

--- src/utils/common.rs
+++ src/utils/common.rs
@@ -318,15 +318,17 @@ pub fn check_deser<T>(
 
 
 #[cfg(any(feature = "tokio", feature = "wasm", feature = "blocking"))]
-pub async fn post_to_nvaapi<
-  V: Serialize,
+pub async fn post_to_nvaapi_cb<
+  // V: Serialize,
   R: for<'de> Deserialize<'de>,
   T
 >(
-  nvacl: &crate::NavAbilityClient,
-  request_body: crate::QueryBody<V>,
+  // nvacl: &crate::NavAbilityClient,
+  // request_body: crate::QueryBody<V>,
   fn_modifier: fn(R) -> T,
-  retries: Option<i32>
+  retries: Option<i32>,
+  post_req: reqwest::RequestBuilder
+  // post_cb: impl Fn() -> Result<reqwest::Response, reqwest::Error>
 ) -> Result<T, Box<dyn Error>> {
   
   // TBD
@@ -338,14 +340,17 @@ pub async fn post_to_nvaapi<
   
   let mut trycount = retries.unwrap_or(3);
   while 0 < trycount {
-    
-    let req_res = nvacl.client
-    .post(&nvacl.apiurl)
-    .json(&request_body)
+
+    let req_res = post_req.try_clone()
+    .expect("Unable to clone request")
     .send().await;
+    // let req_res = post_req.try_clone()
+    // .expect("Unable to clone request")
+    // .json(&request_body).send().await;
+
     
-    if let Err(ref re) = req_res {
-      let erm = format!("API request error for {:?}: {:?}",  type_name::<V>(), &re);
+    if let Err(re) = req_res {
+      let erm = format!("API request error: {:?}", &re);
       to_console_error(&erm);
     } else {
       // generic transport and serde error checks
@@ -359,9 +364,45 @@ pub async fn post_to_nvaapi<
     trycount -= 1;
   }
   return Err(Box::new(crate::GQLRequestError { 
-    details: format!("API request failed after {} retries {:?}", retries.unwrap_or(3), type_name::<V>()).to_owned()
+    details: format!("API request failed after {} retries", retries.unwrap_or(3)).to_owned()
   }));
 }
+
+
+
+#[cfg(any(feature = "tokio", feature = "wasm", feature = "blocking"))]
+pub async fn post_to_nvaapi<
+  V: Serialize,
+  R: for<'de> Deserialize<'de>,
+  T
+>(
+  nvacl: &crate::NavAbilityClient,
+  request_body: crate::QueryBody<V>,
+  fn_modifier: fn(R) -> T,
+  retries: Option<i32>
+) -> Result<T, Box<dyn Error>> {
+
+  let post_req = nvacl.client
+    .post(&nvacl.apiurl)
+    .json(&request_body);
+    // .send();
+  // let cb = || {
+  //     nvacl.client
+  //     .post(&nvacl.apiurl)
+  //     .json(&request_body)
+  //     .send()
+  // };
+
+  return post_to_nvaapi_cb::<R,T>(
+    // nvacl,
+    // request_body,
+    fn_modifier,
+    retries,
+    post_req
+    // cb.await
+  ).await;
+}
+
 
 
 // ====================== FUTURE IDEAS ======================
