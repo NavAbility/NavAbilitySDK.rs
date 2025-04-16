df642e5bcef4014587af8cf3447f7e378994f934
dehann <dehann.fourie@gmail.com>
2025-04-16 11:57:58 -0700

    add jsoncrud util

--- src/utils/common.rs
+++ src/utils/common.rs
@@ -13,8 +13,14 @@ use std::{
   future::Future
 };
 
-#[cfg(any(feature = "tokio", feature = "wasm", feature = "blocking"))]
 use serde::{Serialize,Deserialize};
+
+use serde_json::{
+  Map,
+  Value
+};
+
+use base64::{Engine as _, engine::{self, general_purpose}, alphabet};
 
 // use graphql_client::GraphQLQuery;
 use crate::{
@@ -388,6 +394,62 @@ pub async fn post_to_nvaapi<
     post_req
   ).await;
 }
+
+
+
+// ====================== NodeMetadata ======================
+
+
+#[derive(Default, Clone, Serialize, Deserialize, Debug)]
+pub struct JSONCRUD {}
+
+impl JSONCRUD {
+  pub fn to_jsonstr(
+    jobj: Map<String,Value>,
+  ) -> String {
+    serde_json::to_string(&jobj).unwrap().to_string()
+  }
+  
+  pub fn from_jsonstr(
+    jstr: &str
+  ) -> Map<String,Value> {
+    match serde_json::from_str(jstr) {
+      Err(e) => {
+        to_console_error(&format!("JSONCRUD unable to parse json string {:?}\n {:?}",e,jstr));
+        return Map::<String,Value>::new();
+      },
+      Ok(jsonmap) => {
+        return jsonmap;
+      }
+    }
+  }
+  
+  pub fn to_jsonstr_b64(
+    jobj: Map<String,Value>,
+  ) -> String {
+    return general_purpose::STANDARD.encode(Self::to_jsonstr(jobj).to_string());
+  }
+  
+  pub fn from_jsonstr_b64(
+    jstr_b64: &str
+  ) -> Map<String,Value> {
+    match &general_purpose::STANDARD.decode(jstr_b64) {
+      Ok(jstr) => {
+        Self::from_jsonstr(
+          std::str::from_utf8(
+            jstr
+          ).expect("JSONCRUD, invalid json UTF8 string decoding")
+        )
+      },
+      Err(e) => {
+        to_console_error(&format!("JSONCRUD, base64 decode of b64_string failed {:?}",e));
+        return Map::<String,Value>::new();
+      }
+    }
+  }
+}
+
+
 
 
 
